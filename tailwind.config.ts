64af88661ec4a1357bb3c3a016fae01713fc8759
Christian Trinh <trinhc2@mcmaster.ca>
2025-01-02 18:48:25 -0500

    add skeleton ui and ordered bosses

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,30 @@
+
+import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
+const config = {
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-
-	plugins: []
+	plugins: [
+		skeleton({
+			themes: { preset: [ "wintry" ] }
+		})
+	]
 } satisfies Config;
+
+export default config;
+						
