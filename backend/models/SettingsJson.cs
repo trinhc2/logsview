b28fc4a6b1598c603cd9c35e1ba802323fac34d2
Christian Trinh <trinhc2@mcmaster.ca>
2025-01-14 18:04:39 -0500

    added folder setting functionality

---
+++ backend/models/SettingsJson.cs
@@ -0,0 +1,7 @@
+namespace settings.Models
+{
+    public class JsonData
+        {
+            public required string LogsFolderLocation { get; set; }
+        }
+}
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,8 +1,43 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
 
+    let folderLocation: string = '';
+    let inputText: string = '';
+
+    const backend = 'http://localhost:5163'
+
+    async function getFolderLocation() {
+        const response = await fetch(backend + '/api/Settings/getFolderLocation')
+        if (response.ok) {
+            const data = await response.json()
+            folderLocation = data.logsFolderLocation || '';
+            inputText = folderLocation
+        }
+    }
+
+    async function setFolderLocation() {
+        const response = await fetch(backend + '/api/Settings/setFolderLocation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ LogsFolderLocation: inputText }),
+    });
+
+    if (response.ok) {
+      console.log('Updated JSON');
+    } else {
+      console.error('Failed to save JSON');
+    }
+
+    }
+    onMount(() => {
+		getFolderLocation();
+	});
 </script>
 
 <div>
     Settings Page
-    <input class="input" title="Input (text)" type="text" placeholder="input text" />
+    <input bind:value={inputText} class="input p-2" title="Input (text)" type="text" placeholder="Set LOA Logs location" />
+    <button on:click={setFolderLocation} class="btn">Set</button>
 </div>
